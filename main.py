64b0a970049459ffee2b19094f5055ae6150b00e
Edgardo Gonzalez-Murillo <>
2021-04-30 15:53:54 +0000

    Drawing game map

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ PINK = (139,0,139)
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 
-Maze = [[0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0],
+#testing map
+Maze = [[1,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,1,0,0,0,0,0,0,0,0],
+        [0,0,0,0,1,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0,0,0],
@@ -52,6 +53,16 @@ while not stop:
 
   #render section-----------------------------------------------
   screen.fill((0,0,0))
+
+  for i in range (10): # 
+    for j in range (12):
+      if Maze[i][j]==1:
+        pygame.draw.rect(screen, (255,0,0), (j*50,i*50,50,50))#(top left x, top left y, width, height)
+      #if GameMap[i][j]==2:
+        #pygame.draw.rect(screen, (255,255,0), (j*25,i*25,25,25))#(top left x, top left y, width, height)
+      #if GameMap[i][j]==3:
+        #pygame.draw.rect(screen, (0,255,0), (j*25,i*25,25,25))#(top left x, top left y, width, height)
+
 
   pygame.draw.rect(screen, PINK, (xpos,ypos,20,20))
 
